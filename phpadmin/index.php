260bcc83ada093d34150d178bfc29a9960dab331
Mark Vodyanitskiy <mvodya@gmail.com>
2020-11-13 23:52:57 +1000

    Смещение navbar к центру

--- phpadmin/index.php
+++ phpadmin/index.php
@@ -53,20 +53,20 @@ function drawTree($data)
 <body>
 
   <nav class="navbar navbar-expand navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand">VVSU-Chatbot CP</a>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <!-- <ul class="navbar-nav">
+      <div class="container container-fluid">
+        <a class="navbar-brand">VVSU-Chatbot CP</a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <!-- <ul class="navbar-nav">
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">Дерево ключей</a>
           </li>
         </ul> -->
+        </div>
+        <form method="post" action="/">
+          <input type="hidden" name="a" value="logout">
+          <button type="submit" href="?a=logout" class="btn btn btn-secondary">Выход</button>
+        </form>
       </div>
-      <form method="post" action="/">
-        <input type="hidden" name="a" value="logout">
-        <button type="submit" href="?a=logout" class="btn btn btn-secondary">Выход</button>
-      </form>
-    </div>
   </nav>
 
   <div class="container mt-3">
