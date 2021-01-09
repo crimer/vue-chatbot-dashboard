f0c091b84ca344492bab8f5dac9455687734e32b
Mark Vodyanitskiy <mvodya@gmail.com>
2021-01-09 22:30:57 +1000

    Добавил Sentry

--- phpadmin/config.php
+++ phpadmin/config.php
@@ -1,5 +1,7 @@
 <?php
 
+include 'sentry.php';
+
 $CONFIG['api_url'] = 'http://vvsubotapi.t.frogling.com/api/';
 //$CONFIG['api_key'] = '63px3s';
 if (isset($_COOKIE['api_key'])) {
---
+++ phpadmin/sentry.php
@@ -0,0 +1,9 @@
+<?php
+
+//require 'vendor/autoload.php';
+
+//Sentry\init([
+//  'dsn' => '...',
+//  'traces_sample_rate' => 1.0,
+//  'environment' => 'test',
+//]);
